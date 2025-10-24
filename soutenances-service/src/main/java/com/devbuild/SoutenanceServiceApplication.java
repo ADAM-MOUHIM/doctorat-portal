7504ea2176c5f6ba8795e01e5473fdbc7961b214
Adam Mouhim <adammouhim030@gmail.com>
2025-10-24 09:36:56 +0100

    init NEW

--- soutenances-service/src/main/java/com/devbuild/SoutenanceServiceApplication.java
+++ soutenances-service/src/main/java/com/devbuild/SoutenanceServiceApplication.java
@@ -4,10 +4,12 @@ package com.devbuild;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class SoutenanceServiceApplication {
 
     public static void main(String[] args) {SpringApplication.run(SoutenanceServiceApplication.class, args);}
